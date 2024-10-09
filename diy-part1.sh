08d7f7a76dc08ac5c7015766f6d55a960e638e6c
oppen321 <zj18139624826@gmail.com>
2024-10-10 01:02:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -49,6 +49,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/oppen321/luci-app-easyupdate package/luci-app-easyupdate
 
 # 科学上网插件
 git clone --depth=1 -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
